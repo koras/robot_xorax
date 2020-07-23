4f96ee17e0e431e26cb7184c03bda1d7beabd46a
koras <kos6476@yandex.ru>
2020-07-23 19:45:27 +0300

    Clearing log entries

--- modules/loger.lua
+++ modules/loger.lua
@@ -5,28 +5,28 @@ local logSignal = getScriptPath().."\\log\\EventLog.txt";
 
   -- Пытается открыть файл в режиме "чтения/записи"
 local function save(text) 
-     f = io.open(log,"a");
-     if f == nil then 
-       f = io.open(log,"w"); 
-      f:close();
-      f = io.open(log,"a");
-    end; 
-       f:write(text .. "\n")
-       -- Закрывает файл
-      f:close(); 
+    --  f = io.open(log,"a");
+    --  if f == nil then 
+    --    f = io.open(log,"w"); 
+    --   f:close();
+    --   f = io.open(log,"a");
+    -- end; 
+    --    f:write(text .. "\n")
+    --    -- Закрывает файл
+    --   f:close(); 
 end
 
         -- Пытается открыть файл в режиме "чтения/записи"
 local function saveSignal(text) 
-           f = io.open(logSignal,"a");
-           if f == nil then 
-             f = io.open(logSignal,"w"); 
-            f:close();
-            f = io.open(logSignal,"a");
-          end; 
-             f:write(text .. "\n")
-             -- Закрывает файл
-            f:close(); 
+          --  f = io.open(logSignal,"a");
+          --  if f == nil then 
+          --    f = io.open(logSignal,"w"); 
+          --   f:close();
+          --   f = io.open(logSignal,"a");
+          -- end; 
+          --    f:write(text .. "\n")
+          --    -- Закрывает файл
+          --   f:close(); 
 end
 
  
